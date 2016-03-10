d3b1d5f1d55138d1bdc4081153395ffd6e40404a
alex <lxyxpx@gmail.com>
2016-03-10 18:12:28 +0000

    - ‘time/.#sync.hs’ / ‘time/sync.hs’

--- time/sync.hs
+++ time/sync.hs
@@ -1,11 +1,11 @@
 import qualified Network.Socket as N
-import qualified Sound.OSC.Transport.FD.UDP as O
+-- ximport qualified Sound.OSC.Transport.FD.UDP as O
 import Sound.OSC.FD
 
-set_udp_opt k v (O.UDP s) = N.setSocketOption s k v
-get_udp_opt k (O.UDP s) = N.getSocketOption s k
+set_udp_opt k v (UDP s) = N.setSocketOption s k v
+get_udp_opt k (UDP s) = N.getSocketOption s k
 
-main = do fd <- O.openUDP "127.0.0.1" 57110
+main = do fd <- openUDP "127.0.0.1" 57110
           set_udp_opt N.Broadcast 1 fd
           sendOSC fd $ Message "/test" [string "1 2 3 4 5 8 9"]
 
