e5549428c0155343295f0c51d3c6c1758d976a2f
alex <lxyxpx@gmail.com>
2016-03-10 18:09:48 +0000

    - ‘time/.#sync.hs’ / ‘time/sync.hs’

---
+++ time/sync.hs
@@ -0,0 +1,9 @@
+import qualified Network.Socket as N
+import qualified Sound.OSC.Transport.FD.UDP as O
+
+set_udp_opt k v (O.UDP s) = N.setSocketOption s k v
+get_udp_opt k (O.UDP s) = N.getSocketOption s k
+
+main = do fd <- O.openUDP "127.0.0.1" 57110
+          get_udp_opt N.Broadcast fd
+          set_udp_opt N.Broadcast 1 fd
