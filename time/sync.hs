406d948df3f4825b890e3885b19a772d235ab1c8
alex <lxyxpx@gmail.com>
2016-03-10 18:19:45 +0000

    / ‘time/sync.hs’

--- time/sync.hs
+++ time/sync.hs
@@ -4,7 +4,7 @@ import Sound.OSC.FD
 set_udp_opt k v (UDP s) = N.setSocketOption s k v
 get_udp_opt k (UDP s) = N.getSocketOption s k
 
-main = do fd <- openUDP "127.0.0.1" 57110
+main = do fd <- openUDP "10.14.1.100" 57110
           set_udp_opt N.Broadcast 1 fd
           sendOSC fd $ Message "/test" [string "1 2 3 4 5 8 9"]
 
