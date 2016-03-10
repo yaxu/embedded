ff2776395fbd8265bd5aff62e51ab04b89b52ce6
alex <lxyxpx@gmail.com>
2016-03-10 18:11:39 +0000

    - ‘time/#sync.hs#’ / ‘time/sync.hs’

--- time/sync.hs
+++ time/sync.hs
@@ -5,5 +5,6 @@ set_udp_opt k v (O.UDP s) = N.setSocketOption s k v
 get_udp_opt k (O.UDP s) = N.getSocketOption s k
 
 main = do fd <- O.openUDP "127.0.0.1" 57110
-          get_udp_opt N.Broadcast fd
           set_udp_opt N.Broadcast 1 fd
+          sendOSC fd $ Message "/test" [string "1 2 3 4 5 8 9"]
+          
