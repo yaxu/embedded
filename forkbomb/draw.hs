9b32c2e72ccc7b2798f3f3ff2e4cde883d5cfd05
alex <lxyxpx@gmail.com>
2016-06-13 14:36:15 +0100

    / ‘forkbomb/draw.hs’

--- forkbomb/draw.hs
+++ forkbomb/draw.hs
@@ -7,7 +7,7 @@ h = 300
 main = withImageSurface FormatARGB32 w h $ \surf ->
   do renderWith surf $
        do setOperator OperatorOver
-          setSourceRGB 0 0 0
+          setSourceRGB 1 1 1
           rectangle 0 0 (fromIntegral w) (fromIntegral h)
           fill
           setSourceRGB 1 1 1
