a0d2fe7fb865e7813eb55359ab2fb91364444adc
alex <lxyxpx@gmail.com>
2016-06-15 14:37:46 +0100

    - ‘forkbomb/.#draw.hs’ / ‘forkbomb/draw.hs’

--- forkbomb/draw.hs
+++ forkbomb/draw.hs
@@ -15,7 +15,7 @@ cols = (w `div` cellw)
 main = do handle <- openFile "10-t400.txt" ReadMode
           contents <- hGetContents handle
           let things = rleXY cols contents
-          withPDFSurface "test.pdf" (fromIntegral w) (fromIntegral h) $ \surf -> do
+          withPDFSurface "10-t400.pdf" (fromIntegral w) (fromIntegral h) $ \surf -> do
             renderWith surf $ do
               setOperator OperatorOver
               setSourceRGB 1 1 1
