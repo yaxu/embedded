9c8097a845051bc276449f83b2193d42a945f4d4
alex <lxyxpx@gmail.com>
2016-06-15 15:00:20 +0100

    / ‘forkbomb/10-t400.pdf’ / ‘forkbomb/draw.hs’

--- forkbomb/draw.hs
+++ forkbomb/draw.hs
@@ -12,7 +12,7 @@ cols = (w `div` cellw)
 main = do handle <- openFile "10-t400.txt" ReadMode
           contents <- hGetContents handle
           let things = rleXY cols contents
-              h = ((length contents) `div` cols) * cellh + 2
+              h = ((length contents) `div` cols) * cellh + 10
           withPDFSurface "10-t400.pdf" (fromIntegral w) (fromIntegral h) $ \surf -> do
             renderWith surf $ do
               setOperator OperatorOver
