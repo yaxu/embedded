1013e102933ef91b786dc927270bd8c4717814f3
alex <lxyxpx@gmail.com>
2016-06-15 13:12:12 +0100

    - ‘forkbomb/.#draw.hs’ / ‘forkbomb/draw.hs’

--- forkbomb/draw.hs
+++ forkbomb/draw.hs
@@ -23,7 +23,7 @@ main = do handle <- openFile "10.txt" ReadMode
               fill
               setSourceRGB 0 0 0
               -- drawCells 0 0 (rle contents)
-            surfaceWriteToPNG surf "test.png"
+            surfaceWriteToPDF surf "test.pdf"
 
 
 -- drawCells :: Int -> Int -> [(Int, Int)] -> IO ()
