cf559dd6ed0cdf9e33dfbec2f22e173eb7a5cbef
alex <lxyxpx@gmail.com>
2016-06-15 14:20:14 +0100

    - ‘forkbomb/.#draw.hs’ / ‘forkbomb/draw.hs’

--- forkbomb/draw.hs
+++ forkbomb/draw.hs
@@ -26,6 +26,7 @@ main = do handle <- openFile "10.txt" ReadMode
               mapM_ drawThing things
               return ()
 
+drawThing :: (Int, Int, Int, a)
 drawThing (x,y,len,v) = do rectangle (x+(fromIntegral cellw)) (y+(fromIntegral cellh)) (fromIntegral $ cellw*len) (fromIntegral cellh)
                            fill
                            return ()
