e1a267a61379f3f60356f4bc00e7f47b6fa8d0dc
alex <lxyxpx@gmail.com>
2016-06-15 14:29:52 +0100

    - ‘forkbomb/.#draw.hs’ / ‘forkbomb/draw.hs’

--- forkbomb/draw.hs
+++ forkbomb/draw.hs
@@ -14,9 +14,9 @@ cols = (w `div` cellw)
 
 main = do handle <- openFile "10.txt" ReadMode
           contents <- hGetContents handle
-          putStrLn $ show $ contents
-          hClose handle
           let things = rleXY cols contents
+          hClose handle
+          putStrLn $ show $ contents
           withPDFSurface "test.pdf" (fromIntegral w) (fromIntegral h) $ \surf -> do
             renderWith surf $ do
               setOperator OperatorOver
