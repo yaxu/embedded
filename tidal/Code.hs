6a5961f1c2dc8f573e4cc0140c551407847f4831
alex <lxyxpx@gmail.com>
2016-06-29 22:34:37 +0100

    - ‘tidal/.#Code.hs’ / ‘tidal/Code.hs’

--- tidal/Code.hs
+++ tidal/Code.hs
@@ -14,6 +14,11 @@ wrap :: String -> [String]
 wrap [] = []
 wrap s = ((take 200 s) : (wrap (drop 200 s)))
 
+drawText
+  :: CairoString string =>
+     string
+     -> Sound.Tidal.Context.Pattern (Colour Double)
+     -> IO GHC.IO.Exception.ExitCode
 drawText description pat =
   do let w = 136
          h = 566
