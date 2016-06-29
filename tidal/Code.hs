251139c3840853e5a3dfd29d7956e4a5613f165f
alex <lxyxpx@gmail.com>
2016-06-29 22:36:48 +0100

    - ‘tidal/.#Code.hs’ < ‘tidal/#Code.hs#’ > ‘tidal/Code.hs’

--- tidal/Code.hs
+++ tidal/Code.hs
@@ -21,26 +21,26 @@ drawText
 drawText description pat =
   do let w = 136
          h = 566
-     withSVGSurface ("text.svg") w h $ \surf -> do
-        renderWith surf $ do
+     C.withSVGSurface ("text.svg") w h $ \surf -> do
+        C.renderWith surf $ do
           C.save 
           C.scale (w-20) (h)
           C.setOperator C.OperatorOver
           C.setSourceRGB 0 0 0 
           C.rectangle 0 0 1 1
           C.fill
-          mapM_ renderEvent (events pat)
+          mapM_ C.renderEvent (events pat)
           C.restore 
-          return ()
-          save
-          setSourceRGB 0 0 0
-          selectFontFace "Terminal Dosis" FontSlantNormal FontWeightNormal
-          setFontSize 12
-          rotate (pi/ 2)
-          moveTo 5 (negate (w-15))
-          textPath description
-          fill
-          restore
+          C.return ()
+          C.save
+          C.setSourceRGB 0 0 0
+          C.selectFontFace "Terminal Dosis" FontSlantNormal FontWeightNormal
+          C.setFontSize 12
+          C.rotate (pi/ 2)
+          C.moveTo 5 (negate (w-15))
+          C.textPath description
+          C.fill
+          C.restore
      rawSystem "inkscape" ["--without-gui", "--export-pdf=text.pdf", "text.svg"]
      return ()
 
