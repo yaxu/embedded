9dd33573292b09af3abb45c7224177714d2d53a2
alex <lxyxpx@gmail.com>
2016-06-29 22:25:56 +0100

    + ‘tidal/Code.hs’

---
+++ tidal/Code.hs
@@ -0,0 +1,33 @@
+
+wrap :: String -> [String]
+wrap [] = []
+wrap s = ((take 200 s) : (wrap (drop 200 s)))
+
+drawText description pat =
+  do let w = 136
+         h = 566
+     withSVGSurface ("text.svg") w h $ surf -> do
+        renderWith surf $ do
+          C.save 
+          C.scale (w-20) (h)
+          C.setOperator C.OperatorOver
+          C.setSourceRGB 0 0 0 
+          C.rectangle 0 0 1 1
+          C.fill
+          mapM_ renderEvent (events pat)
+          C.restore 
+          return ()
+          save
+          setSourceRGB 0 0 0
+          selectFontFace "Terminal Dosis" FontSlantNormal FontWeightNormal
+          setFontSize 12
+          rotate (pi/ 2)
+          moveTo 5 (negate (w-15))
+          textPath description
+          fill
+          restore
+     rawSystem "inkscape" ["--without-gui", "--export-pdf=text.pdf", "text.svg"]
+
+
+main = drawText "hello\
+" ("black white")
