90b4c89136fd5a71f9672341da79f0750cddecaf
alex <lxyxpx@gmail.com>
2016-07-02 09:11:43 +0100

    - ‘tidal/.#Light.hs’ / ‘tidal/Light.hs’

--- tidal/Light.hs
+++ tidal/Light.hs
@@ -114,5 +114,6 @@ maximum' xs = maximum xs
 stringToColour' ('b':'d':[]) = red
 stringToColour' ('c':'p':[]) = green
 stringToColour' ('k':'u':'r':'t':[]) = blue
+stringToColour' ('s':'n':[]) = blue
 stringToColour' s = stringToColour s
 stringToColour' s = stringToColour s
