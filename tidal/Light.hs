659225fa0ccbb8fba72cb76e5ae12d3a7ed0d066
alex <lxyxpx@gmail.com>
2016-07-02 09:07:19 +0100

    / ‘tidal/Light.hs’

--- tidal/Light.hs
+++ tidal/Light.hs
@@ -33,7 +33,7 @@ light = Shape {params = [s_p,
                          dur_p
                          ],
                cpsStamp = True,
-               latency = 0.3
+               latency = 0.28
               }
 lightSlang = OscSlang {
   path = "/lightrgb",
