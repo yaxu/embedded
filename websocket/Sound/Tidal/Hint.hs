2f683f708ff5135f695ec1db6c6f37ac209cf10d
alex <lxyxpx@gmail.com>
2016-08-12 10:44:01 +0100

    / ‘websocket/Sound/Tidal/Hint.hs’

--- websocket/Sound/Tidal/Hint.hs
+++ websocket/Sound/Tidal/Hint.hs
@@ -8,5 +8,5 @@ import Language.Haskell.Interpreter as Hint
 hintParamPattern  :: String -> IO (Either InterpreterError ParamPattern)
 hintParamPattern x = Hint.runInterpreter $ do
   Hint.set [languageExtensions := [OverloadedStrings]]
-  Hint.setImports ["Prelude","Sound.Tidal.Context","Sound.OSC.Type","Sound.OSC.Datum","Data.Map"]
+  Hint.setImports ["Prelude","Sound.Tidal.Context"]
   Hint.interpret x (Hint.as::ParamPattern)
