c7fd5109fcc92e5ef2ebb94e649010a300837289
alex <lxyxpx@gmail.com>
2016-08-12 17:47:28 +0100

    / ‘websocket/Sound/Tidal/Hint.hs’

--- websocket/Sound/Tidal/Hint.hs
+++ websocket/Sound/Tidal/Hint.hs
@@ -5,34 +5,6 @@ import Sound.Tidal.Context
 
 import Language.Haskell.Interpreter as Hint
 
-{-import Control.Monad
-import Control.Concurrent
-import Control.Concurrent.MVar
-import Language.Haskell.Interpreter
-import Sound.OSC.FD
-import Sound.OSC.Datum
-
-import Sound.Tidal.Stream
-import Sound.Tidal.Pattern
-import qualified Texture.Types as T
-import Data.Colour
-import Data.Colour.Names
-import Data.Colour.SRGB
-import Data.Hashable
-import Data.Bits
-import Data.Maybe
-import Sound.Tidal.Dirt
-import Sound.Tidal.OscStream
-import qualified Data.Map as Map
-import Control.Applicative
-import qualified Data.ByteString.Char8 as C
-
-import Data.Typeable
-deriving instance Typeable Param
--- deriving instance Typeable1 Pattern
-deriving instance Typeable Sound.OSC.FD.Datum
--}
-
 import Data.Map
 
 data Response = OK {parsed :: ParamPattern}
