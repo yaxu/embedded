cd2fd7669ecc5a9910851212784c6671fb237d41
alex <lxyxpx@gmail.com>
2016-08-13 22:39:51 +0100

    - ‘websocket/Sound/Tidal/.#WebSocket.hs’ / ‘websocket/Sound/Tidal/WebSocket.hs’

--- websocket/Sound/Tidal/WebSocket.hs
+++ websocket/Sound/Tidal/WebSocket.hs
@@ -19,7 +19,7 @@ type TidalState = (Double -> IO (),[Tidal.ParamPattern -> IO()])
 port = 9162
 
 main = do
-  putStrLn "Tidal websocket server, listening on port " ++ show 9162
+  putStrLn "TidalCycles websocket server, listening on port " ++ show 9162
   WS.runServer "0.0.0.0" 9162 $ (\pending -> do
     conn <- WS.acceptRequest pending
     putStrLn "received new connection"
