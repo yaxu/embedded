e6720b881410a3708d2f6251921bbe9887a04e07
alex <lxyxpx@gmail.com>
2016-08-14 23:56:55 +0100

    - ‘websocket/Sound/Tidal/#WebSocket.hs#’ - ‘websocket/Sound/Tidal/.#WebSocket.hs’ / ‘websocket/Sound/Tidal/WebSocket.hs’

--- websocket/Sound/Tidal/WebSocket.hs
+++ websocket/Sound/Tidal/WebSocket.hs
@@ -27,7 +27,7 @@ main = do
   d $ Tidal.sound (Tidal.p "bd sn")
   WS.runServer "0.0.0.0" port $ (\pending -> do
     conn <- WS.acceptRequest pending
-    putStrLn "received new connection"
+    putStrLn $  "received new connection" ++ show conn
     pats <- takeMVar mPatterns
     putStrLn $ "pat count: " ++ show (length pats)
     putMVar mPatterns ((conn, Tidal.silence):pats)
@@ -75,7 +75,7 @@ updatePat :: TidalState -> (WS.Connection, Tidal.ParamPattern) -> IO ()
 updatePat (d, mPatterns) (conn, p) =
   do pats <- takeMVar mPatterns
      putStrLn $ show $ length pats
-     let pats' = ((conn,p) : pats)
+     let pats' = ((conn,p) : filter ((/= conn) . fst) pats)
          ps = map snd pats'
      putStrLn "right then."
      putStrLn $ show $ length pats'
