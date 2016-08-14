76f7f119377c1fd9b71af441370850e95786f459
alex <lxyxpx@gmail.com>
2016-08-14 21:09:10 +0100

    / ‘websocket/Sound/Tidal/WebSocket.hs’

--- websocket/Sound/Tidal/WebSocket.hs
+++ websocket/Sound/Tidal/WebSocket.hs
@@ -22,7 +22,7 @@ main = do
   mPatterns <- newMVar []
   WS.runServer "0.0.0.0" port $ (\pending -> do
     conn <- WS.acceptRequest pending
-        putStrLn "received new connection"
+    putStrLn "received new connection"
     WS.forkPingThread conn 30
     d <- Tidal.dirtStream
     loop (d, mPatterns) conn
