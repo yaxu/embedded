b4ea772ba2a78fb7d86474235733e91bb36194fc
alex <lxyxpx@gmail.com>
2016-08-14 23:38:19 +0100

    - ‘websocket/Sound/Tidal/.#WebSocket.hs’ / ‘websocket/Sound/Tidal/WebSocket.hs’

--- websocket/Sound/Tidal/WebSocket.hs
+++ websocket/Sound/Tidal/WebSocket.hs
@@ -75,10 +75,8 @@ updatePat (d, mPatterns) (conn, p) =
   do pats <- readMVar mPatterns
      let pats' = ((conn,p) : filter ((/= conn) . fst) pats)
          ps = map snd pats'
-     {-
      putMVar mPatterns pats'
-     -}
-     d $ Tidal.stack ps
+     -- d $ Tidal.stack ps
      return ()
      
 {-
