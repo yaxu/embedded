a47f02c15c92ffb46e58c4f621feb8672d31828c
alex <lxyxpx@gmail.com>
2016-08-14 21:08:15 +0100

    / ‘websocket/Sound/Tidal/WebSocket.hs’

--- websocket/Sound/Tidal/WebSocket.hs
+++ websocket/Sound/Tidal/WebSocket.hs
@@ -32,7 +32,7 @@ loop :: TidalState -> WS.Connection -> IO ()
 loop state@(d, mPatterns) conn = do
   msg <- try (WS.receiveData conn)
   -- add to dictionary of connections -> patterns, could use a map for this
-  modifyMVar_ mPatterns (return . ((conn, silence):))
+  modifyMVar_ mPatterns (return . ((conn, Tidal.silence):))
   
   case msg of
     Right s -> do
