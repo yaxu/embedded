e3ee3416b4cda33cde2f091ecd4def2e1892b72e
alex <lxyxpx@gmail.com>
2016-08-14 22:22:40 +0100

    / ‘websocket/Sound/Tidal/WebSocket.hs’

--- websocket/Sound/Tidal/WebSocket.hs
+++ websocket/Sound/Tidal/WebSocket.hs
@@ -36,7 +36,7 @@ loop state@(d, mPatterns) conn = do
   modifyMVar_ mPatterns (\ps -> return ((conn, Tidal.silence):ps))
   case msg of
     Right s -> do
-      act state (T.unpack s)
+      act conn state (T.unpack s)
       loop state conn
     Left WS.ConnectionClosed -> close state "unexpected loss of connection"
     Left (WS.CloseRequest _ _) -> close state "by request from peer"
