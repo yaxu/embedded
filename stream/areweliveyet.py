38bd8e4ebc918213c51d1811c8b0c5ed7276c223
alex <lxyxpx@gmail.com>
2016-08-23 15:00:25 +0100

    - ‘stream/.#areweliveyet.py’ / ‘stream/areweliveyet.py’

--- stream/areweliveyet.py
+++ stream/areweliveyet.py
@@ -56,6 +56,7 @@ def get_info():
     if live:
         return("Live stream: " + title)
     else:
+        # It is probably live, just youtube api is slow to update
         return("Live stream")
 
 def live_mode():
