2e80670efecf4e84a5b047aaf6a192fe805a76e1
alex <lxyxpx@gmail.com>
2016-08-23 13:20:39 +0100

    - ‘stream/.#areweliveyet.py’ / ‘stream/areweliveyet.py’

--- stream/areweliveyet.py
+++ stream/areweliveyet.py
@@ -2,6 +2,7 @@
 
 import json
 
+channel = "UC-id0vwQoAUYBNCm0nmaqQw"
 
 f = open("/home/alex/Dropbox/keys/youtube.txt")
 key = f.read()
@@ -9,3 +10,4 @@ f.close()
 key = key.rstrip()
 print key
 
+"https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=%s&eventType=live&type=video&%s"
