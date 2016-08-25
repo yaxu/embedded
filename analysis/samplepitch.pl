ff0e299050a85daa35841e262116f818a9b4aba8
alex <lxyxpx@gmail.com>
2016-08-25 15:30:50 +0100

    + ‘analysis/samplepitch-output.hs’ + ‘analysis/samplepitch.pl’

---
+++ analysis/samplepitch.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $folder = "/home/alex/Dropbox/projects/dirt/samples";
+
+opendir(my $dh, $folder) or die;
+my @subfolders = grep { (! /^\./) && -d "$folder/$_" } readdir($dh);
+closedir($dh);
+
+my $first = 1;
+
+foreach my $subfolder (@subfolders) {
+    if (!$first) {
+	print ",\n";
+    }
+    else {
+	$first = 0;
+    }
+    opendir(my $dh, "$folder/$subfolder") or die;
+    my @files = grep {/\.wav$/i} readdir($dh);
+    closedir($dh);
+    print "  (\"$subfolder\",\n    [";
+    my @averages;
+    foreach my $file (sort @files) {
+	my $fn = "$folder/$subfolder/" . quotemeta($file);
+	my @output = `aubiopitch $fn`;
+	my $n = 0;
+	my $total = 0;
+	foreach my $line (@output) {
+	    if ($line =~ /(\d+(?:.\d+))\s(\d+(?:.\d+))/) {
+		my ($time, $value) = ($1, $2);
+		if ($value > 0) {
+		    $n++;
+		    $total += $value;
+		}
+	    }
+	}
+	push(@averages, $n ? $total /  $n : 0);
+    }
+    print(join(", ", @averages));
+    print("])")
+}
