930f7cb854f9f6f8f87cbbf9a1988369825e02ed
alex <alex@slab.org>
2016-12-14 23:40:59 +0000

    - ‘musicbox/.#look.py’ / ‘musicbox/look.py’

--- musicbox/look.py
+++ musicbox/look.py
@@ -249,7 +249,8 @@ def runloop():
             cv2.drawContours(frame,thing['contour'],-1, (255,0,0), -1)
             x = thing['centre'][0]
             y = thing['centre'][1]
-            cv2.line(frame, (int(thing['centre'][0]-5), int(thing['centre'][1])), (int(thing['centre'][0]+5), int(thing['centre'][1])), (0,255,0), 1)
+            cv2.line(frame, (int(x-4),int(y)), (int(x+4),int(y)), (0,255,0), 1)
+            cv2.line(frame, (int(x),int(y-4)), (int(x),int(y+4)), (0,255,0), 1)
         im2 = Image.fromarray(numpy.array(frame))
         pg_img = pygame.image.frombuffer(im2.tobytes(), im2.size, im2.mode)
 	screen.blit(pg_img, (0,0))
