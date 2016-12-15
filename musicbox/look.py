b212113fbae5a10f138085383082dcd78094b77b
alex <alex@slab.org>
2016-12-15 15:47:23 +0000

    / ‘musicbox/look.py’

--- musicbox/look.py
+++ musicbox/look.py
@@ -232,7 +232,7 @@ def runloop():
     
     while True:
         ret,grab = cap.read()
-        grab = grab[170:270,140:465]
+        grab = grab[170:270,140:470]
         things = []
 
         orig = grab
