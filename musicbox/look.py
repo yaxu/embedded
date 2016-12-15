8d94c24bd9ba1435904fc6a482f6a325a7d05535
alex <alex@slab.org>
2016-12-15 00:35:25 +0000

    - ‘musicbox/.#look.py’ / ‘musicbox/look.py’

--- musicbox/look.py
+++ musicbox/look.py
@@ -222,19 +222,16 @@ def runloop():
 
         lines = cv2.HoughLines(edges,1,np.pi/180,100)
         
-        if lines:
-            for rho,theta in lines[0]:
-                a = np.cos(theta)
-                b = np.sin(theta)
-                x0 = a*rho
-                y0 = b*rho
-                x1 = int(x0 + 1000*(-b))
-                y1 = int(y0 + 1000*(a))
-                x2 = int(x0 - 1000*(-b))
-                y2 = int(y0 - 1000*(a))
-                cv2.line(orig,(x1,y1),(x2,y2),(0,0,255),2)
-        else:
-            print "no lines."
+        for rho,theta in lines[0]:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 1000*(-b))
+            y1 = int(y0 + 1000*(a))
+            x2 = int(x0 - 1000*(-b))
+            y2 = int(y0 - 1000*(a))
+            cv2.line(orig,(x1,y1),(x2,y2),(0,0,255),2)
 
         for (i, c) in enumerate(contours):
             area = cv2.contourArea(c)
