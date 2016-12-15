9fa8151df229c125f54c0e9bf9b0973a2407a9fc
alex <alex@slab.org>
2016-12-15 11:21:00 +0000

    - ‘musicbox/.#look.py’ / ‘musicbox/look.py’ / ‘musicbox/save.png’

--- musicbox/look.py
+++ musicbox/look.py
@@ -205,7 +205,7 @@ def runloop():
     cap = cv2.VideoCapture(1)
 #    orig = cv2.blur(cv2.imread('image.jpg'), (3,3))
     ret,grab = cap.read()
-    grab = grab[140:440,170:270]
+    grab = grab[170:270,140:440]
     cv2.imwrite('save.png',grab)
 
     while True:
