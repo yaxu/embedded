f9ee255f05740963456759a9ebe6d9067644b41f
alex <alex@slab.org>
2016-12-15 00:23:08 +0000

    - ‘musicbox/.#look.py’ / ‘musicbox/look.py’

--- musicbox/look.py
+++ musicbox/look.py
@@ -266,7 +266,7 @@ def runloop():
             y = thing['y']
             cv2.line(frame, (int(x-4),int(y)), (int(x+4),int(y)), (0,255,0), 1)
             cv2.line(frame, (int(x),int(y-4)), (int(x),int(y+4)), (0,255,0), 1)
-        im2 = Image.fromarray(np.array(orig))
+        im2 = Image.fromarray(np.array(edge))
         pg_img = pygame.image.frombuffer(im2.tobytes(), im2.size, im2.mode)
 	screen.blit(pg_img, (0,0))
         pygame.display.flip()
