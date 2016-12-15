6d494607d0f29cd6fcfd5677fbef27b3ecfb0c80
alex <alex@slab.org>
2016-12-15 11:17:06 +0000

    - ‘musicbox/.#look.py’ / ‘musicbox/look.py’

--- musicbox/look.py
+++ musicbox/look.py
@@ -218,9 +218,9 @@ def runloop():
         contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         print len(contours)
         edges = cv2.Canny(gray,50,100,apertureSize = 3)
-        cv2.imshow('threshold',thresh)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        #cv2.imshow('threshold',thresh)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
 
         #lines = cv2.HoughLines(edges,1,np.pi/180,25,np.pi/2)
         
