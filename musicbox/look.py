765ce8d49e6e4b8740ea7e7c1ac9438603efac22
alex <alex@slab.org>
2016-12-15 00:50:03 +0000

    - ‘musicbox/.#look.py’ / ‘musicbox/look.py’

--- musicbox/look.py
+++ musicbox/look.py
@@ -216,7 +216,7 @@ def runloop():
         contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         print len(contours)
         edges = cv2.Canny(gray,50,100,apertureSize = 3)
-        #cv2.imshow('edges',edges)
+        cv2.imshow('threshold',thresh)
         #cv2.waitKey(0)
         #cv2.destroyAllWindows()
 
