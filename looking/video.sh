46b1ea730e087377dc44a37e9433fffedbaf022e
alex <alex@slab.org>
2017-04-10 15:31:32 +0100

    / ‘looking/video.sh’

--- looking/video.sh
+++ looking/video.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo playing from archive..
-omxplayer -b -o hdmi --font /home/alex/dosis/TerminalDosis-Light.ttf /home/alex/video-archive/*
+omxplayer -o hdmi --font /home/alex/dosis/TerminalDosis-Light.ttf /home/alex/video-archive/*
 
