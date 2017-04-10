e8e1542863eee89259c27e3996cfe5b463c39e43
alex <alex@slab.org>
2017-04-10 15:32:37 +0100

    / ‘looking/video-live.sh’

--- looking/video-live.sh
+++ looking/video-live.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-omxplayer -o hdmi --font /home/alex/dosis/TerminalDosis-Light.ttf --subtitles /home/alex/video-archive/live.srt rtmp://slab.org/livecode/yaxu
+omxplayer -o hdmi -b --font /home/alex/dosis/TerminalDosis-Light.ttf --subtitles /home/alex/video-archive/live.srt rtmp://slab.org/livecode/yaxu
