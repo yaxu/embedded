bf773941d3d21211fe8e0f570177dc5960294d81
alex <alex@slab.org>
2017-05-08 12:23:58 +0100

    / ‘musicbox/look.py’

--- musicbox/look.py
+++ musicbox/look.py
@@ -115,6 +115,7 @@ def runloop():
             if note < 0:
                 note = 0
             thing['note'] = note
+
             #print "area: " + str(area) + " roundness: " + str(roundness)
             if roundness < 1.1 and area < 800: #area > 100 and area < 600 and roundness < 1.1:
                 if thing['x'] < low:
