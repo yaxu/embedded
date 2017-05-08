8842f14d72fc968e0a169a07e6c9351f4220c15d
alex <alex@slab.org>
2017-05-08 12:24:26 +0100

    - ‘musicbox/.#look.py’ / ‘musicbox/look.py’

--- musicbox/look.py
+++ musicbox/look.py
@@ -63,31 +63,10 @@ def runloop():
         orig = grab
         im = cv2.GaussianBlur(orig, (0,0), 3)
         gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-        #thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            #cv2.THRESH_BINARY,11,2)
         ret,thresh = cv2.threshold(gray,threshold,255,0)
         contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        #print len(contours)
-        edges = cv2.Canny(gray,50,100,apertureSize = 3)
-        #cv2.imshow('threshold',thresh)
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
 
-        #lines = cv2.HoughLines(edges,1,np.pi/180,25,np.pi/2)
-        
-        #for rho,theta in lines[0]:
-        #    print theta
-        #      
-        #    a = np.cos(theta)
-        #    b = np.sin(theta)
-        #    x0 = a*rho
-        #    y0 = b*rho
-        #    x1 = int(x0 + 1000*(-b))
-        #    y1 = int(y0 + 1000*(a))
-        #    x2 = int(x0 - 1000*(-b))
-        #    y2 = int(y0 - 1000*(a))
-        #    if theta < 0.1:
-        #      cv2.line(orig,(x1,y1),(x2,y2),(0,0,255),2)
+        edges = cv2.Canny(gray,50,100,apertureSize = 3)
 
         for (i, c) in enumerate(contours):
             area = cv2.contourArea(c)
