6688583384c033c2c1819090dcb0115fbe67c4ae
alex <alex@slab.org>
2017-05-09 11:35:14 +0200

    - ‘tidal/.#newvis.hs’ / ‘tidal/newvis.hs’ / ‘tidal/test.pdf’ / ‘tidal/test.svg’

--- tidal/newvis.hs
+++ tidal/newvis.hs
@@ -43,8 +43,8 @@ renderLevel total (n, level) = do C.save
                  C.setSourceRGBA 0.6 0.6 0.6 1
                  C.rectangle (x + half) y lineW levelHeight
                  C.withLinearPattern r g b 256.0 $ \pattern ->
-                   do C.patternAddColorStopRGBA pattern 0 0 0 0 1
-                      C.patternAddColorStopRGBA pattern 1 1 1 1 0
+                   do C.patternAddColorStopRGB pattern 0 0 0 0
+                      C.patternAddColorStopRGB pattern 1 1 1 1
                       C.patternSetFilter pattern C.FilterFast
                       C.setSource pattern
                       -- C.setSourceRGBA r g b 1
