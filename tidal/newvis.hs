2f61de2585b8a65740b36c5286bccab51ea282ef
alex <alex@slab.org>
2017-05-09 11:35:36 +0200

    / ‘tidal/newvis.hs’

--- tidal/newvis.hs
+++ tidal/newvis.hs
@@ -45,7 +45,7 @@ renderLevel total (n, level) = do C.save
                  C.withLinearPattern r g b 256.0 $ \pattern ->
                    do C.patternAddColorStopRGB pattern 0 0 0 0
                       C.patternAddColorStopRGB pattern 1 1 1 1
-                      C.patternSetFilter pattern C.FilterFast
+                      -- C.patternSetFilter pattern C.FilterFast
                       C.setSource pattern
                       -- C.setSourceRGBA r g b 1
                       C.fill
