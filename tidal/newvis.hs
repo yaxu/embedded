1e4f286b4df2cddbbf496467b6615f876828ec9f
alex <alex@slab.org>
2017-05-09 17:24:52 +0200

    / ‘tidal/newvis.hs’

--- tidal/newvis.hs
+++ tidal/newvis.hs
@@ -48,7 +48,7 @@ renderLevel total (n, level) = do C.save
                       C.save
                       C.patternAddColorStopRGBA pattern 0 r g b 1
                       C.patternAddColorStopRGBA pattern 1 r g b 0.25
-                      C.patternSetFilter pattern C.FilterFast
+                      -- C.patternSetFilter pattern C.FilterFast
                       C.setSource pattern
                       -- C.setSourceRGBA r g b 1
                       --C.arc (x+half) (y+half) (w/2) 0 (2 * pi)
