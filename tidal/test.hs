4767c172b723f2eed42f949462a2055991cf5c6e
alex <alex@slab.org>
2017-05-23 12:40:39 +0100

    / ‘tidal/test.hs’

--- tidal/test.hs
+++ tidal/test.hs
@@ -2,6 +2,6 @@
 
 import Sound.Tidal.Context
 
-let x = fast 12 $ chunk 4 (fast 2) $ iter 4 ("bd bd bd" :: Pattern String)
+x = fast 12 $ chunk 4 (fast 2) $ iter 4 ("bd bd bd" :: Pattern String)
 
 main = putStrLn $ show $ length (arc x (0,1))
