ebf41369a95dec1b90f58ff278ab676e3b5dfab6
alex <alex@slab.org>
2017-05-23 12:41:37 +0100

    - ‘tidal/.#test.hs’ / ‘tidal/test.hs’

--- tidal/test.hs
+++ tidal/test.hs
@@ -1,7 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 import Sound.Tidal.Context
 
-x = fast 12 $ chunk 4 (fast 2) $ iter 4 ("bd bd bd" :: Pattern String)
+x = fast 12 $ chunk 4 (fast 2) $ iter 4 ((p "bd bd bd") :: Pattern String)
 
 main = putStrLn $ show $ length (arc x (0,1))
