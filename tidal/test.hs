174b61083344de908fc603a332daa4c54c1662bc
alex <alex@slab.org>
2017-05-24 10:15:31 +0100

    - ‘tidal/.#test.hs’ / ‘tidal/test.hs’

--- tidal/test.hs
+++ tidal/test.hs
@@ -2,4 +2,4 @@ import Sound.Tidal.Context
 
 x = fast 12 $ chunk 4 (fast 2) $ iter 4 y
 y = (p "bd bd bd") :: Pattern String
-main = putStrLn $ show $ length $ concatMap (\n -> arc x (n,n+0.25)) [0, 0.25, 0.5,0.75]
+main = putStrLn $ show $ length (arc x (0,1))
