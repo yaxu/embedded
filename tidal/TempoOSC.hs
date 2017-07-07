51117be6c5c7f645958d5d9a6b76fd2139b4a8f6
alex <alex@slab.org>
2017-07-07 23:52:24 +0100

    - ‘tidal/.#TempoOSC.hs’ / ‘tidal/TempoOSC.hs’

--- tidal/TempoOSC.hs
+++ tidal/TempoOSC.hs
@@ -3,6 +3,10 @@ module Sound.Tidal.TempoOSC where
 import Sound.OSC.FD
 import Network.Socket
 
+serverPort :: IO Int
+serverPort =
+   maybe 9160 (readNote "port parse") <$> lookupEnv "TIDAL_TEMPO_PORT"
 
 listen = udpServer iNADDR_ANY aNY_PORT
 
+server = do udpServer iNADDR_ANY 
