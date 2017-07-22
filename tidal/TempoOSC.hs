5c4e7f7dc653fa243aafb16cca50afc4038c7fe5
alex <alex@slab.org>
2017-07-22 13:28:22 +0200

    - ‘tidal/.#TempoOSC.hs’ / ‘tidal/TempoOSC.hs’

--- tidal/TempoOSC.hs
+++ tidal/TempoOSC.hs
@@ -14,7 +14,7 @@ serverPort =
 runServer = do port <- serverPort
                -- inaddr_any + any_port
                sock <- N.socket N.AF_INET N.Datagram 0
-               N.setSocketOption sock (N.NoDelay  , 1)
+               N.setSocketOption sock N.NoDelay 1
                N.setSocketOption sock (N.ReuseAddr, 1)
                N.setSocketOption sock (N.ReusePort, 1)
                a <- N.inet_addr "0.0.0.0"
