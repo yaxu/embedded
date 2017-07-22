b2530418feed29a2532c435a00685ec453217d56
alex <alex@slab.org>
2017-07-22 13:29:35 +0200

    / ‘tidal/TempoOSC.hs’

--- tidal/TempoOSC.hs
+++ tidal/TempoOSC.hs
@@ -15,7 +15,7 @@ runServer = do port <- serverPort
                -- inaddr_any + any_port
                sock <- N.socket N.AF_INET N.Datagram 0
                -- N.setSocketOptiSocketon sock N.NoDelay 1
-               -- N.setSocketOption sock N.ReuseAddr 1
+               N.setSocketOption sock N.ReuseAddr 1
                N.setSocketOption sock N.ReusePort 1
                a <- N.inet_addr "0.0.0.0"
                let sa = N.SockAddrInet (fromIntegral port) a
