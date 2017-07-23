d8a2ae45c6b532ebb758951f0afaadaa8629f3e5
alex <alex@slab.org>
2017-07-23 22:47:07 +0200

    / ‘tidal/TempoOSC.hs’

--- tidal/TempoOSC.hs
+++ tidal/TempoOSC.hs
@@ -103,7 +103,7 @@ beatNow t = do now <- getCurrentTime
 
 clockedTick :: Int -> (Tempo -> Int -> IO ()) -> IO ()
 clockedTick tpb callback = 
-  do (mTempo, mCps, _) <- tempoReceiver
+  do (mTempo, _, _) <- tempoReceiver
      t <- readMVar mTempo
      now <- getCurrentTime
      let delta = realToFrac $ diffUTCTime now (at t)
